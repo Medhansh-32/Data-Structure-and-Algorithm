550bf40849fadab78b58c4ba674d474d29392eea
Medhansh-32 <sharmamedhansh21@gmail.com>
2025-02-23 02:00:46 +0530

    Subarry sum == K(- and +)

--- arrays/easy/Subarry_Sum_K.java
+++ arrays/easy/Subarry_Sum_K.java
@@ -5,25 +5,27 @@ import java.util.Map;
 
 public class Subarry_Sum_K {
 
-    public static  void subArraySum(int arr[],int k){
-        int sum=0;
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-            map.put(sum,i);
+    public static int findAllSubarraysWithGivenSum(int arr[], int k) {
+        int n = arr.length;
+        Map<Integer,Integer> mpp = new HashMap<>();
+        int preSum = 0, cnt = 0;
 
-            if(sum==k){
-                System.out.println(0+" "+i);
-            }
-            if(map.containsKey(sum-k)){
-                System.out.println(map.get(sum-k)+1+" "+i);
-            }
+        mpp.put(0, 1);
+        for (int i = 0; i < n; i++) {
 
+            preSum += arr[i];
+            int remove = preSum - k;
+            cnt += mpp.getOrDefault(remove, 0);
+            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
         }
+        return cnt;
     }
 
     public static void main(String[] args) {
 
-        subArraySum(new int[]{10,5,2,7,1,5},15);
+        System.out.println(findAllSubarraysWithGivenSum(new int[]{10,5,2,7,1,5},15));
+        System.out.println(findAllSubarraysWithGivenSum(new int[]{3, 4, -7, 1, 3, 3, 1, -4}, 7));
+
+
     }
 }
