83a439b3e670b753a16ed130966ae17e93f55cab
Medhansh-32 <sharmamedhansh21@gmail.com>
2025-02-25 22:54:14 +0530

    [Kadane's Alog,Majority]

---
+++ arrays/medium/Majority_Element.java
@@ -0,0 +1,29 @@
+package arrays.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Majority_Element {
+
+    public static int majorityElement(int[] nums) {
+        int majorityElement=0,count=0;
+        majorityElement=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                majorityElement=nums[i];
+            }
+
+            if(nums[i]==majorityElement){
+                count++;
+            }else{
+                count--;
+            }
+
+        }
+        return majorityElement;
+    }
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{3,2,3}));
+    }
+}
