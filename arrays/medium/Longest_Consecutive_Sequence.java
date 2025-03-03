aa29049ce68fd2088b646496fcb4d9f1df43f73a
Medhansh-32 <sharmamedhansh21@gmail.com>
2025-03-03 23:58:58 +0530

    [LCS and Set Zeroes]

---
+++ arrays/medium/Longest_Consecutive_Sequence.java
@@ -0,0 +1,34 @@
+package arrays.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Longest_Consecutive_Sequence {
+
+    static public int longestConsecutive(int[] nums) {
+
+        Set<Integer> set=new HashSet<>();
+        int lcs=0,max=0;
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        for(Integer i:set){
+            if(set.contains(i-1)){
+                continue;
+            }else{
+                lcs=1;
+                int k=i;
+                while(set.contains(k+1)){
+                    k++;
+                    lcs++;
+                }
+                max=Math.max(lcs,max);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestConsecutive(new int[]{100,4,200,1,3,2}));
+    }
+}
---
+++ arrays/medium/Set_Zeroes.java
@@ -0,0 +1,74 @@
+package arrays.medium;
+
+public class Set_Zeroes
+{
+    static int[][] zeroMatrix(int[][] matrix, int n, int m) {
+
+        int col0 = 1;
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+
+                    matrix[i][0] = 0;
+
+
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        // Initialize matrix
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+
+        int[][] ans = zeroMatrix(matrix, n, m);
+
+
+        System.out.println("The Final matrix is: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
