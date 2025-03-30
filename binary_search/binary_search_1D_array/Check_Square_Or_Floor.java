7771765616462c48d972abdfff08f9761a3fc13a
Medhansh-32 <sharmamedhansh21@gmail.com>
2025-03-30 20:13:12 +0530

    [Square root or nearest numbser to Square root]

---
+++ binary_search/binary_search_1D_array/Check_Square_Or_Floor.java
@@ -0,0 +1,31 @@
+package binary_search.binary_search_1D_array;
+
+public class Check_Square_Or_Floor {
+
+    static  int floorSqrt(int n) {
+
+        int org=n;
+        int low=1;
+        int ans=0;
+        if (n*n==n) return n;
+
+        while(low<=n){
+            int mid=(low+n)/2;
+
+            if(mid*mid==org) return mid;
+
+            if(mid*mid>org){
+                n=mid-1;
+            }else{
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Square root or nearest number to square root is : "+floorSqrt(74));
+
+    }
+}
