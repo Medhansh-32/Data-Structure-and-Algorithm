fdf5e1567fc9ed32c1dba98e3c244a1f35337b22
Medhansh-32 <sharmamedhansh21@gmail.com>
2025-04-19 23:58:56 +0530

    Median in a row-wise sorted Matrix

--- binary_search/binary_search_2D_Arrays/Peak_Element.java
+++ binary_search/binary_search_2D_Arrays/Peak_Element.java
@@ -1,49 +1,64 @@
 package binary_search.binary_search_2D_Arrays;
 
 public class Peak_Element {
-    public static int[] findPeakGrid(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
 
-        int left = 0;
-        int right = cols - 1;
+        public static int max(int[][] mat, int col) {
+            int index = 0;
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < mat.length; i++) {
+                if (mat[i][col] > max) {
+                    index = i;
+                    max = mat[i][col];
+                }
 
-        while (left <= right) {
-            int midCol = left + (right - left) / 2;
-            int maxRow = findMaxInColumn(mat, midCol);
-
-            boolean leftIsBigger = midCol - 1 >= 0 && mat[maxRow][midCol - 1] > mat[maxRow][midCol];
-            boolean rightIsBigger = midCol + 1 < cols && mat[maxRow][midCol + 1] > mat[maxRow][midCol];
-
-            if (!leftIsBigger && !rightIsBigger) {
-                return new int[]{maxRow, midCol}; // Peak found
-            } else if (leftIsBigger) {
-                right = midCol - 1; // Move left
-            } else {
-                left = midCol + 1; // Move right
             }
+            return index;
         }
 
-        return new int[]{-1, -1}; // No peak found (shouldn't happen)
-    }
+        public static int[] findPeakGrid(int[][] mat) {
 
-    private static int findMaxInColumn(int[][] mat, int col) {
-        int maxRow = 0;
-        for (int i = 0; i < mat.length; i++) {
-            if (mat[i][col] > mat[maxRow][col]) {
-                maxRow = i;
+            int row1 = max(mat, 0);
+            if (mat[0].length == 1) {
+                return new int[] { max(mat, 0), 0 };
             }
+
+            if (mat[row1][0] > mat[row1][1]) {
+                return new int[] { row1, 0 };
+            }
+            int row2 = max(mat, mat[0].length - 1);
+            if (mat[row2][mat[0].length - 1] > mat[row2][mat[0].length - 2]) {
+                return new int[] { row2, mat[0].length - 1 };
+            }
+
+            int start = 1, end = mat[0].length - 2;
+
+            while (start <= end) {
+
+                int col = (start + end) / 2;
+
+                int row = max(mat, col);
+
+                int val = mat[row][col];
+
+                if (val > mat[row][col - 1] && val > mat[row][col + 1]) {
+                    return new int[] { row, col };
+                } else if (val < mat[row][col - 1]) {
+                    end = col - 1;
+                } else {
+                    start = col + 1;
+                }
+            }
+
+            return new int[] { -1, -1 };
         }
-        return maxRow;
-    }
 
     // Example usage
     public static void main(String[] args) {
         int[][] matrix = {
                 {10, 8, 10, 10},
                 {14, 13, 12, 11},
-                {15, 9, 11, 21},
-                {16, 17, 19, 20}
+                {15, 90, 11, 1},
+                {16, 17, 19, 2}
         };
 
         int[] peak = findPeakGrid(matrix);
