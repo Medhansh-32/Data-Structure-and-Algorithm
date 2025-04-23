856ca1147d62ce833e38a9069edbdd3c737991d0
Medhansh-32 <sharmamedhansh21@gmail.com>
2025-04-23 20:12:36 +0530

    [Anagram Check]

---
+++ strings/basic_and_easy/Anagram_Check.java
@@ -0,0 +1,27 @@
+package strings.basic_and_easy;
+
+public class Anagram_Check {
+
+    static  public boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length())
+            return false;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < str1.length(); i++) {
+            freq[str1.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            freq[str2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        String Str1 = "INTEGER".toLowerCase();
+        String Str2 = "TEGERNI".toLowerCase();
+        System.out.println(isAnagram(Str1, Str2));
+    }
+}
