ce4a0c9041224f064784afb0257f89f5ca58d54f
Medhansh-32 <sharmamedhansh21@gmail.com>
2025-04-26 22:05:54 +0530

    [Check if String is Rotated or not]

---
+++ strings/basic_and_easy/Rotation_Check.java
@@ -0,0 +1,16 @@
+package strings.basic_and_easy;
+
+public class Rotation_Check {
+    static  public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+
+    }
+    public static void main(String[] args) {
+        String s = "abcd";
+        String goal = "abcd";
+        System.out.println(rotateString(s, goal));
+    }
+}
